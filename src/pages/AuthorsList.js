c5841c4282e44e1c9bd97bb12d981db0e1f44263
jbrasko <jbrasko0508@gmail.com>
2021-09-22 13:03:20 -0400

    testing axios get

--- src/pages/AuthorsList.js
+++ src/pages/AuthorsList.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/Authors'
+})
 
 const AuthorsList = () => {
+  useEffect(() => {
+    api.get('/').then(res => {
+      console.log(res)
+    })
+  }, [])
+
   return <h1>Authors List</h1>
 }
 
