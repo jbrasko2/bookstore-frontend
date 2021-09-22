baa8be5666032575fe16cbeb81f183cf0a0b510c
jbrasko <jbrasko0508@gmail.com>
2021-09-22 13:32:11 -0400

    fetch book data in useEffect

--- src/pages/BooksList.js
+++ src/pages/BooksList.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const BooksList = () => {
-  return <h1>Books List</h1>
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const result = await axios('http://localhost:3000/books')
+      setBooks(result.data)
+    }
+    getBooks()
+  }, [])
+
+  return (
+    <>
+      <h1>Books List</h1>
+      <ul>
+        {books.map(book => {
+          return <li key={book._id}>{book.title}</li>
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default BooksList
