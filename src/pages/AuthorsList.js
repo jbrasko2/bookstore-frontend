ea7e825895aa36ae19cfc023d859c9f88b3fb303
jbrasko <jbrasko0508@gmail.com>
2021-09-22 13:30:17 -0400

    fetch author data in useEffect

--- src/pages/AuthorsList.js
+++ src/pages/AuthorsList.js
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000/Authors'
-})
-
 const AuthorsList = () => {
+  const [authors, setAuthors] = useState([])
+
   useEffect(() => {
-    api.get('/').then(res => {
-      console.log(res)
-    })
+    const getAuthors = async () => {
+      const result = await axios('http://localhost:3000/authors')
+      setAuthors(result.data)
+    }
+    getAuthors()
   }, [])
 
-  return <h1>Authors List</h1>
+  return (
+    <>
+      <h1>Authors List</h1>
+      <ul>
+        {authors.map(author => {
+          return <li key={author._id}>{author.name}</li>
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default AuthorsList
