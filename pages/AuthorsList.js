6d542136f3aa08c08aaab38dfc01bd14d95bfb48
jbrasko <jbrasko0508@gmail.com>
2021-09-22 12:51:05 -0400

    install axios, create data instance

--- pages/AuthorsList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AuthorsList = () => {
-  return <h1>Authors List</h1>
-}
-
-export default AuthorsList
--- pages/BooksList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const BooksList = () => {
-  return <h1>Books List</h1>
-}
-
-export default BooksList
