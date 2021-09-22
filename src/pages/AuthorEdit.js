6d5a902da6515530c4f752f4ae1fee997ad0cfd6
jbrasko <jbrasko0508@gmail.com>
2021-09-22 19:58:16 -0400

    create AuthorEdit

---
+++ src/pages/AuthorEdit.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import AuthorForm from '../components/AuthorForm'
+import axios from 'axios'
+
+const AuthorEdit = ({ history }) => {
+  const [author, setAuthor] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getAuthor = async () => {
+      const result = await axios(`http://localhost:3000/authors/${id}`)
+      setAuthor(result.data)
+    }
+    getAuthor()
+  }, [id])
+
+  const renderPage = () => {
+    return (
+      <>
+        <h1>Edit {author.name}</h1>
+        <AuthorForm handleSubmit={submitEdit} author={author} />
+      </>
+    )
+  }
+
+  const renderLoad = () => {
+    return <p>..loading</p>
+  }
+
+  const submitEdit = async ({ name, dob }) => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:3000/Authors/${id}`,
+        {
+          name,
+          dob,
+        }
+      )
+      console.log('Returned Data:', response)
+      history.goBack()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return author ? renderPage() : renderLoad()
+}
+
+export default AuthorEdit
