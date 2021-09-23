d4b36a32e50fbbc27b7689671f78071dc636e5d0
jbrasko <jbrasko0508@gmail.com>
2021-09-22 21:27:08 -0400

    add header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
+    <h1>Welcome to The Bookstore</h1>
       <Link to='/books'><button>Books List</button></Link>
       <Link to='/authors'><button>Authors List</button></Link>
     </div>
