0e8a925b23f89f205aca4ba4355c72b601017692
jbrasko <jbrasko0508@gmail.com>
2021-09-22 23:00:02 -0400

    add styling

--- src/pages/AuthorNew.js
+++ src/pages/AuthorNew.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AuthorForm from '../components/AuthorForm'
 import axios from 'axios'
+import styled from 'styled-components'
 
 const AuthorNew = ({ history }) => {
   const submitNew = async ({ name, dob }) => {
@@ -17,13 +18,17 @@ const AuthorNew = ({ history }) => {
   }
 
   return (
-    <>
+    <Wrapper>
       <h1>Create New Author</h1>
       <AuthorForm handleSubmit={submitNew} />
       <br />
       <button onClick={history.goBack}>Back</button>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`
 
 export default AuthorNew
--- src/pages/BookNew.js
+++ src/pages/BookNew.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookForm from '../components/BookForm'
 import axios from 'axios'
+import styled from 'styled-components'
 
 const BookNew = ({ history }) => {
   const submitNew = async ({ title, authorName, year }) => {
@@ -18,13 +19,17 @@ const BookNew = ({ history }) => {
   }
 
   return (
-    <>
+    <Wrapper>
       <h1>Create New Book</h1>
       <BookForm handleSubmit={submitNew} />
       <br />
       <button onClick={history.goBack}>Back</button>
-    </>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`
 
 export default BookNew
