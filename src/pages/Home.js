e124c9e0df56df18c1a120581e47c7c4419127ff
jbrasko <jbrasko0508@gmail.com>
2021-09-22 21:58:41 -0400

    add styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
     <div>
-    <h1>Welcome to The Bookstore</h1>
-      <Link to='/books'><button>Books List</button></Link>
-      <Link to='/authors'><button>Authors List</button></Link>
+      <Header>Welcome to The Bookstore</Header>
+      <ButtonWrapper>
+        <Link to='/books'>
+          <Button>Books List</Button>
+        </Link>
+        <Link to='/authors'>
+          <Button>Authors List</Button>
+        </Link>
+      </ButtonWrapper>
     </div>
   )
 }
+
+const Header = styled.h1`
+  text-align: center;
+`
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+`
+
+const Button = styled.button`
+  margin: 12px;
+`
 
 export default Home
