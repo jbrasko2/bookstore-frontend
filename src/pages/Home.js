f625c09f40a7d7dd475b99ac77b6782e75ec6fca
jbrasko <jbrasko0508@gmail.com>
2021-09-23 00:29:36 -0400

    add bookshelf png

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import bookshelf from '../bookshelf.png'
 
 const Home = () => {
   return (
     <div>
       <Header>Welcome to The Bookstore</Header>
+      <Bookshelf src={bookshelf} alt='bookshelf' />
       <ButtonWrapper>
         <Link to='/books'>
           <Button>Books List</Button>
@@ -28,6 +30,12 @@ const ButtonWrapper = styled.div`
 
 const Button = styled.button`
   margin: 12px;
+`
+
+const Bookshelf = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 200px;
 `
 
 export default Home
