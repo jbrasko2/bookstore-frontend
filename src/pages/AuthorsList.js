460d14426650ef6bdb29aa0a84832c4487ce39e8
jbrasko <jbrasko0508@gmail.com>
2021-10-07 16:59:08 -0400

    add loader while fetching authors

--- src/pages/AuthorsList.js
+++ src/pages/AuthorsList.js
@@ -6,18 +6,24 @@ import styled from 'styled-components';
 
 const AuthorsList = () => {
   const [authors, setAuthors] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const getAuthors = async () => {
-      const result = await axios('https://jb-bookstore-server.herokuapp.com/authors');
+      const result = await axios(
+        'https://jb-bookstore-server.herokuapp.com/authors'
+      );
       setAuthors(result.data);
+      setLoaded(true);
     };
     getAuthors();
   }, []);
 
   const deleteAuthor = async id => {
     try {
-      await axios.delete(`https://jb-bookstore-server.herokuapp.com/Authors/${id}`);
+      await axios.delete(
+        `https://jb-bookstore-server.herokuapp.com/Authors/${id}`
+      );
       setAuthors(authors.filter(author => author._id !== id));
     } catch (err) {
       console.log(err);
@@ -38,47 +44,55 @@ const AuthorsList = () => {
       .catch(err => console.log(err));
   };
 
-  return (
-    <Wrapper>
-      <h1>Authors List</h1>
-      <SearchBar handleSubmit={searchAuthors} />
-      <ListWrapper>
-        <ul>
-          {authors.map(author => {
-            return (
-              <ListItem key={author._id}>
-                <div>
-                  <Link to={'/authors/' + author._id}>
-                    {author.name} ({author.books.length}{' '}
-                    {author.books.length === 1 ? 'book' : 'books'}) - Born{' '}
-                    {author.dob.split('T')[0]}{' '}
-                  </Link>
-                </div>
-                <div>
-                  <Link to={`/authors/${author._id}/edit`}>
-                    <button>Edit</button>
-                  </Link>
-                  <button onClick={() => deleteAuthor(author._id)}>
-                    Delete
-                  </button>
-                </div>
-              </ListItem>
-            );
-          })}
-        </ul>
-      </ListWrapper>
-      <Link to={'/authors/new'}>
-        <button>Create New Author</button>
-      </Link>
-      <br />
-      <Link to='/'>
-        <button>Home</button>
-      </Link>
-      <Link to='/books'>
-        <button>Books List</button>
-      </Link>
-    </Wrapper>
-  );
+  const renderPage = () => {
+    return (
+      <Wrapper>
+        <h1>Authors List</h1>
+        <SearchBar handleSubmit={searchAuthors} />
+        <ListWrapper>
+          <ul>
+            {authors.map(author => {
+              return (
+                <ListItem key={author._id}>
+                  <div>
+                    <Link to={'/authors/' + author._id}>
+                      {author.name} ({author.books.length}{' '}
+                      {author.books.length === 1 ? 'book' : 'books'}) - Born{' '}
+                      {author.dob.split('T')[0]}{' '}
+                    </Link>
+                  </div>
+                  <div>
+                    <Link to={`/authors/${author._id}/edit`}>
+                      <button>Edit</button>
+                    </Link>
+                    <button onClick={() => deleteAuthor(author._id)}>
+                      Delete
+                    </button>
+                  </div>
+                </ListItem>
+              );
+            })}
+          </ul>
+        </ListWrapper>
+        <Link to={'/authors/new'}>
+          <button>Create New Author</button>
+        </Link>
+        <br />
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+        <Link to='/books'>
+          <button>Books List</button>
+        </Link>
+      </Wrapper>
+    );
+  };
+
+  const renderLoad = () => {
+    return <h3>Loading...</h3>;
+  };
+
+  return loaded ? renderPage() : renderLoad();
 };
 
 const Wrapper = styled.div`
